2bfafa2cdec7cf3ab13bbcc4d43784c5cf7d99a0
kenny-io <ekeneeze3@gmail.com>
2022-03-07 17:14:53 +0100

    initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <p>This is the about page</p>
+    <router-link to="/">Home Page</router-link>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <h3>{{ name }}</h3>
+    <h2>{{ student.name }}</h2>
+    <h2>{{ student.school }}</h2>
+    <button @click="changeName">Change Name</button>
+    <router-link to="/about">About Page</router-link>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive } from "@vue/reactivity";
+const name = ref("Kenny");
+
+const student = reactive({
+  name: "Ekene",
+  school: "Alt School",
+});
+
+const changeName = () => {
+  name.value = "Ekene Eze";
+};
+</script>
